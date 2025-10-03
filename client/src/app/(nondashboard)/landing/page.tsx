b6affa2f1cc9b9381f5649f358b8c9f63a07e4b0
FarhanHaider999 <haiderfarhan178@gmail.com>
2025-10-03 11:47:58 +0500

    backend setup

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
+import FooterSection from "./FooterSection";
 
 const Landing = () => {
   return (
     <div>
-      <HeroSection/>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection/>
     </div>
   );
 };
